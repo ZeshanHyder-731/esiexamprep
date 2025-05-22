4fbe816f01807ff1c80d72ad61a65a573f9bc1c8
Zeshan Hyder <zeshan@ut.ee>
2025-05-22 20:19:40 +0300

    test

--- demo/src/main/java/testRestApi.java
+++
@@ -1,9 +0,0 @@
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class testRestAPI {
-    @GetMapping("/esi")
-    public String helloWorld(){
-    return "Welcome to the ESI course!";
-    }
-}
